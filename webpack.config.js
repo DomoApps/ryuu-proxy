7cc2bd60c1e60d955705cdb8ba836eb1c850c4cb
Alex Nelson <w.alexnelson@gmail.com>
2017-07-25 11:19:30 -0600

    remove webpack-config dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,80 @@
-const getDefaultConfig = require('@appteam6/webpack-config');
-const loaders = require('@appteam6/webpack-config/helpers/loaders');
-
 const path = require('path');
 const webpack = require('webpack');
+const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+
 const pkg = require('./package.json');
 
 const ON_PROD = process.env.NODE_ENV === 'production';
 const ON_TEST = process.env.NODE_ENV === 'test';
 const ON_DEV = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV;
 
-const prodPlugins = [];
-const devPlugins = [
-  new webpack.SourceMapDevToolPlugin({ filename: null, test: /\.tsx?$/ }),
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NamedModulesPlugin()
-];
-
-module.exports = (env = {}) => {
-  let config = getDefaultConfig(env);
-
-  // overrides of default config
-  config.target = 'node';
-  config.entry = './index.ts';
-  config.output.filename = 'bundle.js';
-  config.output.sourceMapFilename = 'bundle.js.map';
-
-  config.module.rules = [
-    loaders.tslint,
-    loaders.json, {
-      test: /\.ts(x?)$/,
-      exclude: /node_modules/,
-      loader: 'awesome-typescript-loader',
-      options: {
-        useTranspileModule: true
-      }
-    }
+module.exports = () => {
+  const prodPlugins = [];
+  const devPlugins = [
+    new webpack.SourceMapDevToolPlugin({ filename: null, test: /\.tsx?$/ }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
   ];
 
-  config.plugins = []
+  const plugins = []
     .concat(ON_PROD ? prodPlugins : [])
     .concat(ON_DEV ? devPlugins : []);
 
-  return config;
+  return {
+    context: path.resolve(process.cwd(), './src'),
+
+    entry: './index.ts',
+
+    target: 'node',
+
+    resolve: {
+      modules: ['node_modules'],
+      extensions: ['.ts', '.js'],
+      plugins: [new TsConfigPathsPlugin()]
+    },
+
+    output: {
+      path: path.join(process.cwd(), 'dist'),
+      publicPath: '/',
+      filename: 'bundle.js',
+      sourceMapFilename: 'bundle.js.map',
+      libraryTarget: 'commonjs'
+    },
+
+    devtool: ON_PROD ? 'source-map' : 'cheap-module-eval-source-map',
+
+    plugins: plugins,
+
+    module: {
+      rules: [
+
+        // tslint
+        {
+          enforce: 'pre',
+          test: /\.(j|t)s(x?)$/,
+          exclude: /node_modules/,
+          loader: 'tslint-loader',
+          options: {
+            typeCheck: true
+          }
+        },
+
+        // typescript
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          loader: 'awesome-typescript-loader',
+          options: {
+            useTranspileModule: true
+          }
+        },
+
+        // json
+        {
+          test: /\.json$/,
+          loader: 'json-loader'
+        }
+      ]
+    },
+  }
 };
