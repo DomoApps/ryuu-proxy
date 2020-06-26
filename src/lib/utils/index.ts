3398dfae9062fa16a9a69e09d0e43bafc71ecafc
John Morgan Nemelka <john.morgan.nemelka@gmail.com>
2020-06-26 10:37:51 -0600

    scopes inclusion in configstore variables

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -17,7 +17,7 @@ export function getMostRecentLogin() {
   });
   const loginData = fs.readJsonSync(recent);
 
-  const refreshToken = configstore.get(loginData.instance)
+  const refreshToken = configstore.get(loginData.instance);
   loginData.refreshToken = refreshToken;
   return Promise.resolve(loginData);
 }
@@ -42,8 +42,8 @@ export function getOauthTokens(proxyId: string, scopes: string[] | undefined): P
         : (['domoapps']);
 
       return Promise.all([
-        configstore.get(`${instance}-${proxyId}-accessToken`),
-        configstore.get(`${instance}-${proxyId}-refreshToken`),
+        configstore.get(`${instance}-${proxyId}-${allScopes.join('-')}-accessToken`),
+        configstore.get(`${instance}-${proxyId}-${allScopes.join('-')}-refreshToken`),
       ]);
     })
     .then((tokens: [string, string]) => ({ access: tokens[0], refresh: tokens[1] }));
