b0fc31690ddeca957010260c7fe19e7abecec2b2
John Morgan Nemelka <john.morgan.nemelka@gmail.com>
2020-08-05 09:17:30 -0600

    reconfiguring of configstore

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,23 +2,22 @@ import * as Domo from 'ryuu-client';
 import * as glob from 'glob';
 import * as fs from 'fs-extra';
 import * as Promise from 'core-js/features/promise';
+import * as Configstore from 'configstore';
 
-import { configstore } from './configstore';
 import { OAUTH_ENABLED } from '../constants';
 import { OauthToken, Manifest } from '../models';
 
 export function getMostRecentLogin() {
   const home = Domo.getHomeDir();
-  const logins = glob.sync(home + '/login/*.json');
+  const logins = glob.sync(home + '*.json');
   if (logins.length === 0) return Promise.resolve({});
 
   const recent = logins.reduce((prev, next) => {
     return fs.statSync(prev).mtime > fs.statSync(next).mtime ? prev : next;
   });
   const loginData = fs.readJsonSync(recent);
-
-  const refreshToken = configstore.get(loginData.instance);
-  loginData.refreshToken = refreshToken;
+  const configstore = new Configstore(home + '/' + loginData.instance + '.json');
+  loginData.refreshToken = configstore.get('refreshToken');
   return Promise.resolve(loginData);
 }
 
@@ -33,7 +32,8 @@ export const getProxyId = (manifest: Manifest): string =>
 export function getOauthTokens(proxyId: string, scopes: string[] | undefined): Promise<OauthToken> {
   return getMostRecentLogin()
     .then((loginData) => {
-      const instance = loginData.instance;
+      const home = Domo.getHomeDir();
+      const configstore = new Configstore(home + '/' + loginData.instance + '.json');
       const allScopes = (scopes !== undefined)
         ? ([
           'domoapps',
@@ -42,8 +42,8 @@ export function getOauthTokens(proxyId: string, scopes: string[] | undefined): P
         : (['domoapps']);
 
       return Promise.all([
-        configstore.get(`${instance}-${proxyId}-${allScopes.join('-')}-accessToken`),
-        configstore.get(`${instance}-${proxyId}-${allScopes.join('-')}-refreshToken`),
+        configstore.get(`${proxyId}-${allScopes.join('-')}-accessToken`),
+        configstore.get(`${proxyId}-${allScopes.join('-')}-refreshToken`),
       ]);
     })
     .then((tokens: [string, string]) => ({ access: tokens[0], refresh: tokens[1] }));
